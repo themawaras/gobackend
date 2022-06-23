78935d0c376126ef41d65b4b0454e7d3e4d1a71e
Ferdinand Thema Warastratama <fthema@192.168.1.2>
2022-06-23 09:32:15 +0700

    info : add build

--- handlerRequest.go
+++ handlerRequest.go
@@ -1,49 +1,49 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"net/http"
+// import (
+// 	"encoding/json"
+// 	"fmt"
+// 	"net/http"
 
-	"github.com/gorilla/mux"
-)
+// 	"github.com/gorilla/mux"
+// )
 
-type vehicles struct {
-	Name string
-	Type string
-	Year int
-}
+// type vehicles struct {
+// 	Name string
+// 	Type string
+// 	Year int
+// }
 
-func handlerReqs() {
-	mainRoute := mux.NewRouter()
+// func handlerReqs() {
+// 	mainRoute := mux.NewRouter()
 
-	mainRoute.HandleFunc("/", sampleHandler).Methods("GET")
-	mainRoute.HandleFunc("/user/{id}", paramsHandler).Methods("GET")
-	mainRoute.HandleFunc("/product", queryHandler).Methods("GET")
-	mainRoute.HandleFunc("/vehicle", bodyHandler).Methods("POST")
-}
+// 	mainRoute.HandleFunc("/", sampleHandler).Methods("GET")
+// 	mainRoute.HandleFunc("/user/{id}", paramsHandler).Methods("GET")
+// 	mainRoute.HandleFunc("/product", queryHandler).Methods("GET")
+// 	mainRoute.HandleFunc("/vehicle", bodyHandler).Methods("POST")
+// }
 
-func sampleHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from HOME"))
-}
+// func sampleHandler(w http.ResponseWriter, r *http.Request) {
+// 	w.Write([]byte("Hello from HOME"))
+// }
 
-func paramsHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
+// func paramsHandler(w http.ResponseWriter, r *http.Request) {
+// 	vars := mux.Vars(r)
 
-	fmt.Fprintf(w, "params : %v", vars["id"])
-}
+// 	fmt.Fprintf(w, "params : %v", vars["id"])
+// }
 
-func queryHandler(w http.ResponseWriter, r *http.Request) {
-	vars := r.URL.Query()
+// func queryHandler(w http.ResponseWriter, r *http.Request) {
+// 	vars := r.URL.Query()
 
-	fmt.Fprintf(w, "params : %v", vars["name"])
-}
+// 	fmt.Fprintf(w, "params : %v", vars["name"])
+// }
 
-func bodyHandler(w http.ResponseWriter, r *http.Request) {
-	var vehicle vehicles
-	json.NewDecoder(r.Body).Decode(&vehicle)
+// func bodyHandler(w http.ResponseWriter, r *http.Request) {
+// 	var vehicle vehicles
+// 	json.NewDecoder(r.Body).Decode(&vehicle)
 
-	fmt.Println(vehicle.Name)
+// 	fmt.Println(vehicle.Name)
 
-	// fmt.Fprintf(w, "params : %v", vars["name"])
-}
+// 	// fmt.Fprintf(w, "params : %v", vars["name"])
+// }
--- subRoute.go
+++ subRoute.go
@@ -1,32 +1,32 @@
 package main
 
-import (
-	"net/http"
+// import (
+// 	"net/http"
 
-	"github.com/gorilla/mux"
-)
+// 	"github.com/gorilla/mux"
+// )
 
-func subRoute() {
-	mainRoute := mux.NewRouter()
+// func subRoute() {
+// 	mainRoute := mux.NewRouter()
 
-	users := mainRoute.PathPrefix("/user").Subrouter()
-	users.HandleFunc("/", userHandler)
+// 	users := mainRoute.PathPrefix("/user").Subrouter()
+// 	users.HandleFunc("/", userHandler)
 
-	products := mainRoute.PathPrefix("/product").Subrouter()
-	products.HandleFunc("/", productHandler)
+// 	products := mainRoute.PathPrefix("/product").Subrouter()
+// 	products.HandleFunc("/", productHandler)
 
-	vehicles := mainRoute.PathPrefix("/vehicle").Subrouter()
-	vehicles.HandleFunc("/", vehiclesHandler)
-}
+// 	vehicles := mainRoute.PathPrefix("/vehicle").Subrouter()
+// 	vehicles.HandleFunc("/", vehiclesHandler)
+// }
 
-func userHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from USER"))
-}
+// func userHandler(w http.ResponseWriter, r *http.Request) {
+// 	w.Write([]byte("Hello from USER"))
+// }
 
-func productHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from PRODUCT"))
-}
+// func productHandler(w http.ResponseWriter, r *http.Request) {
+// 	w.Write([]byte("Hello from PRODUCT"))
+// }
 
-func vehiclesHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from VEHICLE"))
-}
+// func vehiclesHandler(w http.ResponseWriter, r *http.Request) {
+// 	w.Write([]byte("Hello from VEHICLE"))
+// }
